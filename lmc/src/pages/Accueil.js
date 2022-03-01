d00d92d3efe2606e7fbd64d5d074e2ff59a88788
Orrigine <valentin@wallmatt.com>
2022-03-02 00:28:40 +0100

    renaming merch-project to strapi

--- lmc/src/pages/Accueil.js
+++ lmc/src/pages/Accueil.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 
 class Accueil extends Component {
-  render() {
-    return (
-      <p>hellloooo ?
-      </p>
-    );
-  }
+    render() {
+        return ( 
+            <h1> Hey this is me ! Your best friend ! ui </h1>
+            
+            
+
+        );
+    }
 }
 
 export default Accueil;
