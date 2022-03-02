b1dcfc4cd4ae0accb30e308412d428e54228f5b7
Orrigine <valentin@wallmatt.com>
2022-03-02 11:53:31 +0100

    Adding materialize react

---
+++ lmc/src/pages/Account.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+
+class Account extends Component {
+  render() {
+    return (
+      <p>Votre compte sur le mauvais coin:
+      </p>
+      
+    );
+  }
+}
+
+export default Account;
