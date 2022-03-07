5a60706ac80944b360be5f06953e5c771fd35a18
Orrigine <valentin@wallmatt.com>
2022-03-07 13:54:47 +0100

    Changing Footer

--- lmc/src/pages/Cart.js
+++ lmc/src/pages/Cart.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SearchNavbar from '../components/MyNavbar';
 import Footer from '../components/Footer'
+import { Row, Col } from "react-bootstrap";
+import Article from "../components/Article";
 
 class Cart extends Component {
 
@@ -8,12 +10,22 @@ class Cart extends Component {
   render() {
     return (
         <>
-            <SearchNavbar/>
-                
+        <SearchNavbar/>
+        
+        
+        <Row>
+          <Col sm="2" /><Col sm="8">
+            <Row>
+              {this.props.cart.length === 0 ?<>
+                  <p>No article in cart</p>
+                </>
+                : this.props.cart.map((article, i) => <Article addArticleToCart={this.props.addArticleToCart} remArticleFromCart={this.props.remArticleFromCart} data={article} inCart={true} />)
+              }
+            </Row>
+          </Col>
+        </Row>
 
-
-
-            <Footer/>
+        <Footer/>
         </>
     );
   }
