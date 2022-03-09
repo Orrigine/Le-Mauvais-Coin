0148545313d99805056332e657583b7763d6b5dc
Wazy42 <lvocanson@gaming.tech>
2022-03-09 12:24:41 +0100

    beautiful cards in browse and cart

--- lmc/src/pages/Cart.js
+++ lmc/src/pages/Cart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SearchNavbar from '../components/MyNavbar';
 import Footer from '../components/Footer'
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Alert } from "react-bootstrap";
 import Article from "../components/ArticleCard";
 
 class Cart extends Component {
@@ -16,9 +16,13 @@ class Cart extends Component {
         <Row>
           <Col sm="2" /><Col sm="8">
             <Row>
-              {this.props.cart.length === 0 ?<>
-                  <p>No article in cart</p>
-                </>
+              {this.props.cart.length === 0 ?
+                <Alert variant="danger">
+                  <Alert.Heading>Your cart is empty!</Alert.Heading>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis odio excepturi optio pariatur tempora, error culpa architecto molestias magnam alias animi ullam, vero officia rem eos quibusdam et. Quam, illum!
+                  </p>
+                </Alert>
                 : this.props.cart.map((tuple, i) => <Article addArticleToCart={this.props.addArticleToCart} remArticleFromCart={this.props.remArticleFromCart} getNumberOfArticle={this.props.getNumberOfArticle} data={tuple.article} viewFromCart={true} />)
               }
             </Row>
